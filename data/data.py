8f48161074220fd981ae7fd61f38e573dae297fc
DongjunLee <ehdwns2045@gmail.com>
2016-05-15 14:17:40 +0900

    Add load_category_dataset with Refactoring

--- data/data.py
+++ data/data.py
@@ -1,146 +1,143 @@
 try:
-   import cPickle as pickle
+    import cPickle as pickle
 except:
     import pickle
 import os
 from PIL import Image
 import numpy as np
 
+GEOMETRY = (256, 84)
+PATCH_GEOMETRY = (84, 84)
+PATCH_COUNT = 5
+
 def load_gender_dataset():
-	# Return dataset - numpy array, label 
-	# 0 : Femail / 1 : Male
+    # Return dataset - numpy array, label
+    # 0 : Femail / 1 : Male
 
-	gender_path = "/home/dj/HighFashionProject/image_analysis/gender_data/scaled_84x256/"
-	geometry = (256, 84)
+    gender_path = "/home/dj/HighFashionProject/image_analysis/gender_data/scaled_84x256/"
 
-	patch_geometry = (84, 84)
-	patch_count = 5
+    # Make Female & Male DataSet
+    female_path = gender_path + "img_female/"
+    female_list = os.listdir(female_path)
+    female_count = len(female_list) * PATCH_COUNT
 
-	# Make Female & Male DataSet
-	female_path = gender_path + "img_female/"
-	female_list = os.listdir(female_path)
-	female_count = len(female_list) * patch_count
-	
-	male_path = gender_path + "img_male/"
-	male_list = os.listdir(male_path)
-	male_count = len(male_list) * patch_count
+    male_path = gender_path + "img_male/"
+    male_list = os.listdir(male_path)
+    male_count = len(male_list) * PATCH_COUNT
 
-	img_data = np.empty((female_count+male_count, patch_geometry[0], patch_geometry[1], 3))
-	label_data = np.zeros((female_count+male_count, 2), dtype="uint8")
+    img_data = np.empty((female_count+male_count, PATCH_GEOMETRY[0], PATCH_GEOMETRY[1], 3))
+    label_data = np.zeros((female_count+male_count, 2), dtype="uint8")
 
-	print "Load Female DataSet....."
-	for i in range(len(female_list)):
-		patch_images = generate_patches(img2numpy_arr(female_path+female_list[i]))
-		for j in range(patch_count):
-			img_data[(i*patch_count)+j,:,:,:] = patch_images[j]	
+    print "Load Female DataSet....."
+    for i in range(len(female_list)):
+        patch_images = generate_patches(img2numpy_arr(female_path+female_list[i]))
+        for j in range(PATCH_COUNT):
+            img_data[(i*PATCH_COUNT)+j,:,:,:] = patch_images[j]
 
-	label_data[:female_count, 0] = 1
-	print "Complete!"
+    label_data[:female_count, 0] = 1
+    print "Complete!"
 
-	print "Load Male DataSet ..."
-	for i in range(len(male_list)):
-		patch_images = generate_patches(img2numpy_arr(male_path+male_list[i]))
-		for j in range(patch_count):
-			img_data[female_count+(i*patch_count)+j,:,:,:] = patch_images[j]	
-	label_data[female_count+1:, 1] = 1
-	print "Complete!"				
+    print "Load Male DataSet ..."
+    for i in range(len(male_list)):
+        patch_images = generate_patches(img2numpy_arr(male_path+male_list[i]))
+        for j in range(PATCH_COUNT):
+            img_data[female_count+(i*PATCH_COUNT)+j,:,:,:] = patch_images[j]
+    label_data[female_count+1:, 1] = 1
+    print "Complete!"
 
-	dataset = {
-		'data' : img_data,
-		'label' : label_data,
-		'geometry': patch_geometry,
-		'num_classes': 2
-	}
+    dataset = {
+        'data' : img_data,
+        'label' : label_data,
+        'geometry': PATCH_GEOMETRY,
+        'num_classes': 2
+    }
 
-	print "dataset : ", img_data.shape, " label : ", label_data.shape
-	return dataset 
+    print "dataset : ", img_data.shape, " label : ", label_data.shape
+    return dataset
 
 def load_category_dataset():
-	# Return dataset - numpy array, label 
-	# Labels => 
-	#	0: Street / 1: Casual / 2: Sexy / 3: Unique / 4: Work wear / 5: Classic
+    # Return dataset - numpy array, label
+    # Labels =>
+    #	0: Street / 1: Casual / 2: Sexy / 3: Unique / 4: Work Wear / 5: Classic
 
-	gender_path = "/home/dj/HighFashionProject/image_analysis/category_data/scaled_84x256/"
-	geometry = (256, 84)
+    num_classes = 6
+    category_path = "/home/dj/HighFashionProject/image_analysis/category_data/scaled_84x256/"
 
-	patch_geometry = (84, 84)
-	patch_count = 5
+    street_path = category_path + "img_street/";      street_label = 0
+    casual_path = category_path + "img_casual/";      casual_label = 1
+    sexy_path = category_path + "img_sexy/";          sexy_label = 2
+    unique_path = category_path + "img_unique/";      unique_label = 3
+    workwear_path = category_path + "img_workwear/";  workwear_label = 4
+    classic_path = category_path + "img_classic/";    classic_label = 5
 
-	# Make Female DataSet
-	female_path = gender_path + "img_female/"
-	female_list = os.listdir(female_path)
-	female_count = len(female_list) * patch_count
-	
-	male_path = gender_path + "img_male/"
-	male_list = os.listdir(male_path)
-	male_count = len(male_list) * patch_count
+    path_label_list = [(street_path, street_label), (casual_path, casual_label), (sexy_path, sexy_label),
+                       (unique_path, unique_label), (workwear_path, workwear_label), (classic_path, classic_label)]
+    dataset_list = map(lambda (p,l): dir2arr(p, l, num_classes))
+    total_count = sum(map(lambda (img,label): label.shape[0], dataset_list))
 
-	img_data = np.empty((female_count+male_count, patch_geometry[0], patch_geometry[1], 3))
-	label_data = np.zeros((female_count+male_count, 2), dtype="uint8")
+    img_data = np.empty((total_count, PATCH_GEOMETRY[0], PATCH_GEOMETRY[1], 3))
+    label_data = label_data = np.zeros((total_count, num_classes), dtype="uint8")
 
-	print "Load Female DataSet....."
-	for i in range(len(female_list)):
-		patch_images = generate_patches(img2numpy_arr(female_path+female_list[i]))
-		for j in range(patch_count):
-			img_data[(i*patch_count)+j,:,:,:] = patch_images[j]	
+    index = 0
+    for i in range(num_classes):
+        imgs, labels = dataset[i]
+        data_count = label.shape[0]
 
-	label_data[:female_count, 0] = 1
-	print "Complete!"
+        img_data[index:index+data_count, :, :, :] = imgs
+        label_data[index:index+data_count, :, :, :] = label_data
 
-	# Make Male DataSet
+        index += data_count
 
-	print "Load Male DataSet ..."
-	for i in range(len(male_list)):
-		patch_images = generate_patches(img2numpy_arr(male_path+male_list[i]))
-		for j in range(patch_count):
-			img_data[female_count+(i*patch_count)+j,:,:,:] = patch_images[j]	
-	label_data[female_count+1:, 1] = 1
-	print "Complete!"				
+    dataset = {
+        'data' : img_data,
+        'label' : label_data,
+        'geometry': PATCH_GEOMETRY,
+        'num_classes': num_classes
+    }
 
-	dataset = {
-		'data' : img_data,
-		'label' : label_data,
-		'geometry': patch_geometry,
-		'num_classes': 2
-	}
+    print "dataset : ", img_data.shape, " label : ", label_data.shape
+    return dataset
 
-	print "dataset : ", img_data.shape, " label : ", label_data.shape
+def dir2arr(dir_path, label_value, num_classes):
+    dir_list = os.listdir(dir_path)
+    dir_count = len(dir_list)*PATCH_COUNT
 
-#	print "Save to Pickle data for cache"
-#	save2p(dataset, cache_path)
-	return dataset 
+    img_data = np.empty((dir_count, PATCH_GEOMETRY[0], PATCH_GEOMETRY[1], 3))
+    label_data = np.zeros((dir_count, num_classes), dtype="uint8")
 
+    for i in range(len(dir_list)):
+        patch_images = generate_patches(img2numpy_arr(dir_path+dir_list[i]))
+        for j in range(PATCH_COUNT):
+            img_data[(i*PATCH_COUNT)+j,:,:,:] = patch_images[j]
+
+    targets = np.full((dir_count), label_value, dtype="uint8")
+    label_data[np.arange(targets.shape[0]), targets] = 1
+    return (img_data, label_data)
 
 def img2numpy_arr(img_path):
-	return np.array(Image.open(img_path))
+    return np.array(Image.open(img_path))
 
 def generate_patches(ndarr):
-	geometry = (256, 84)
-	patch_geometry = (84, 84)
-	patch_count = 5
-
-	step = (geometry[0] - patch_geometry[0]) / (patch_count-1)
-	return [ndarr[i*step:i*step+patch_geometry[0], :, : ] for i in range(patch_count)]
+    step = (GEOMETRY[0] - PATCH_GEOMETRY[0]) / (PATCH_COUNT-1)
+    return [ndarr[i*step:i*step+PATCH_GEOMETRY[0], :, : ] for i in range(PATCH_COUNT)]
 
 def save2p(data, fname):
-	try:
-		with open(fname, "wb") as fh:
-			pickle.dump(data, fh)
-	except IOError as ioerr:
-		print ("File Error: %s" % str(ioerr))
-	except pickle.PickleError as pklerr:
-		print ("Pickle Error: %s" % str(pklerr))
+    try:
+        with open(fname, "wb") as fh:
+            pickle.dump(data, fh)
+    except IOError as ioerr:
+        print ("File Error: %s" % str(ioerr))
+    except pickle.PickleError as pklerr:
+        print ("Pickle Error: %s" % str(pklerr))
 
 def load(fname):
-	savedItems = []
-	try:
-		with open(fname, "rb") as fh:
-			savedItems = pickle.load(fh)
-	except IOError as ioerr:
-		save2p(savedItems, fname)
-	except pickle.PickleError as pklerr:
-		print ("Pickle Error: %s" % str(pklerr))
-	finally:
-		return savedItems
-
-
+    savedItems = []
+    try:
+        with open(fname, "rb") as fh:
+            savedItems = pickle.load(fh)
+    except IOError as ioerr:
+        save2p(savedItems, fname)
+    except pickle.PickleError as pklerr:
+        print ("Pickle Error: %s" % str(pklerr))
+    finally:
+        return savedItems
