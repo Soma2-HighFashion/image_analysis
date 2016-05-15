8587d1d00d60f92143894262be45f0772bfb52ed
DongjunLee <ehdwns2045@gmail.com>
2016-05-15 21:54:21 +0900

    Add train/test Category,Gender DataSet With VGG net

---
+++ train_category_vgg.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+""" Very Deep Convolutional Networks for Large-Scale Visual Recognition.
+Applying VGG 16-layers convolutional network to Oxford's 17 Category Flower
+Dataset classification task.
+References:
+    Very Deep Convolutional Networks for Large-Scale Image Recognition.
+    K. Simonyan, A. Zisserman. arXiv technical report, 2014.
+Links:
+    http://arxiv.org/pdf/1409.1556
+"""
+
+from __future__ import division, print_function, absolute_import
+
+import sys
+sys.path.append('./data')
+import data
+
+dataset = data.load_category_dataset()
+X = dataset['data']
+y = dataset['label']
+
+import tflearn as tl
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.estimator import regression
+
+# Data loading and preprocessing
+#import tflearn.datasets.oxflower17 as oxflower17
+#X, Y = oxflower17.load_data(one_hot=True)
+
+# Building 'VGG Network'
+network = input_data(shape=[None, 84, 84, 3])
+
+network = conv_2d(network, 64, 3, activation='relu')
+network = conv_2d(network, 64, 3, activation='relu')
+network = max_pool_2d(network, 2, strides=2)
+
+network = conv_2d(network, 128, 3, activation='relu')
+network = conv_2d(network, 128, 3, activation='relu')
+network = max_pool_2d(network, 2, strides=2)
+
+network = conv_2d(network, 256, 3, activation='relu')
+network = conv_2d(network, 256, 3, activation='relu')
+network = conv_2d(network, 256, 3, activation='relu')
+network = max_pool_2d(network, 2, strides=2)
+
+network = conv_2d(network, 512, 3, activation='relu')
+network = conv_2d(network, 512, 3, activation='relu')
+network = conv_2d(network, 512, 3, activation='relu')
+network = max_pool_2d(network, 2, strides=2)
+
+network = conv_2d(network, 512, 3, activation='relu')
+network = conv_2d(network, 512, 3, activation='relu')
+network = conv_2d(network, 512, 3, activation='relu')
+network = max_pool_2d(network, 2, strides=2)
+
+network = fully_connected(network, 4096, activation='relu')
+network = dropout(network, 0.5)
+network = fully_connected(network, 4096, activation='relu')
+network = dropout(network, 0.5)
+network = fully_connected(network, 6, activation='softmax')
+
+network = regression(network, optimizer='rmsprop',
+                     loss='categorical_crossentropy',
+                     learning_rate=0.00001)
+
+# Training
+model = tl.DNN(network, checkpoint_path='vgg_category_model', max_checkpoints=1, tensorboard_verbose=3)
+model.fit(X, y, n_epoch=50, shuffle=True,
+          show_metric=True, batch_size=32, snapshot_step=500,
+          snapshot_epoch=False, run_id='vgg_category')
